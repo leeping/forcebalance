986eb8daeb14fe5b6c6335bba033687b7c506c14
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2011-12-21 23:55:00 +0000

    Added a script and updated the logo
    git-svn-id: https://simtk.org/svn/forcebalance@4 e93b7d5b-8bb4-0410-82c6-a20ae092c748

---
+++ bin/ReadForceField.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"@package ReadForceField Read force field from a file and print information out."
+
+from forcebalance.forcefield import FF
+from forcebalance.nifty import printcool
+from sys import argv
+import os
+
+def main():
+    ## Set some basic options.  Note that 'forcefield' requires 'ffdir'
+    ## which indicates the relative path of the force field.
+    options = {'forcefield':argv[1:],
+               'ffdir':''}
+    MyFF = FF(options)
+
+if __name__ == "__main__":
+    main()
