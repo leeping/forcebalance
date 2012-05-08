1d74e695c1ef867d50ace2e283d86995d1dee5c5
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2012-05-08 13:27:30 +0000

    Updated version to 0.12
    git-svn-id: https://simtk.org/svn/forcebalance@59 e93b7d5b-8bb4-0410-82c6-a20ae092c748

--- bin/OptimizePotential.py
+++ bin/OptimizePotential.py
@@ -9,7 +9,7 @@ from forcebalance.project import Project
 
 def main():
     """ Instantiate a ForceBalance project and call the optimizer. """
-    print "\x1b[1;97m Welcome to ForceBalance version 0.11.0! =D\x1b[0m"
+    print "\x1b[1;97m Welcome to ForceBalance version 0.12! =D\x1b[0m"
     if len(sys.argv) != 2:
         print "Please call this program with only one argument - the name of the input file."
         sys.exit(1)
