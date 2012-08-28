8c400345bbb71f5c105c4836f8c4152a2005db0c
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2012-08-28 08:51:49 +0000

    Updated some documentation Improved first example
    git-svn-id: https://simtk.org/svn/forcebalance@125 e93b7d5b-8bb4-0410-82c6-a20ae092c748

--- studies/001_water12_tip4p/test-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-paste <(grep -A1 Step 01_bfgs_from_start.out | awk '/^ +[0-9]/') <(cat <(grep -A1 Step 02a_bfgs_writechk.out | awk '/^ +[0-9]/') <(grep -A1 Step 02b_bfgs_readchk.out | awk '/^ +[1-9]/')) 
-#| awk '{p+=($5 - $11)**2} END {print p}'
-
-paste <(grep -A1 Step 01_bfgs_from_start.out | awk '/^ +[0-9]/') <(cat <(grep -A1 Step 02a_bfgs_writechk.out | awk '/^ +[0-9]/') <(grep -A1 Step 02c_bfgs_readmvals.out | awk '/^ +[1-9]/'))
