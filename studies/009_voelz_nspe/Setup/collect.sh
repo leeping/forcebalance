722cd562065e10919b5c9a8d5ec8e0928be3d5c9
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2012-10-04 00:56:19 +0000

    Added example for Vince Voelz
    git-svn-id: https://simtk.org/svn/forcebalance@150 e93b7d5b-8bb4-0410-82c6-a20ae092c748

---
+++ studies/009_voelz_nspe/Setup/collect.sh
@@ -0,0 +1,9 @@
+tar xjf structures.tar.bz2
+for i in `ls structures/*.xyz | awk -F '.o' '{print $1}' | awk -F 'c.' '{print $NF}' | sort -n | uniq`; do
+    for j in `ls structures/*.xyz | awk -F 'o.' '{print $NF}' | awk -F '.' '{print $1}' | sort -n | uniq`; do
+	f=structures/min.c.$i.o.$j.xyz
+	if [ -f $f ]; then
+	    cat $f >> all.xyz
+	fi
+    done
+done
