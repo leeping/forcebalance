16e55faf9e61d816c2ad353e873a9df56fcc64c8
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2012-11-21 11:59:59 +0000

    Added files for contact
    git-svn-id: https://simtk.org/svn/forcebalance@188 e93b7d5b-8bb4-0410-82c6-a20ae092c748

---
+++ ext/contact/contact.h
@@ -0,0 +1,7 @@
+#ifndef _CPY_CONTACT_H
+#define _CPY_CONTACT_H
+
+void atomic_contact(const float *xyzlist, const int *contacts, int num_contacts, int traj_length, int num_atoms, float *results);
+void closest_contact(const float *xyzlist, const int *residues, const int num_residues, const int residue_width, const int* atoms_per_residue,  const int *contacts, int num_contacts, int traj_length, int num_atoms, float *results);
+
+#endif
