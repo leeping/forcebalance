d941d3a49f09c8fa3b1915aabc969c83787af7a1
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2013-03-14 18:32:58 +0000

    Modified runcuda.sh to not make backup files
    git-svn-id: https://simtk.org/svn/forcebalance@273 e93b7d5b-8bb4-0410-82c6-a20ae092c748

--- studies/2013_Mar_OpenMM_Workshop/targets/dms-liquid/runcuda.sh
+++ studies/2013_Mar_OpenMM_Workshop/targets/dms-liquid/runcuda.sh
@@ -9,7 +9,7 @@
 # module load cuda
 # module load cudatoolkit
 # export OPENMM_CUDA_COMPILER=`which nvcc`
-export BAK=$HOME/temp/runcuda-backups
+# export BAK=$HOME/temp/runcuda-backups
 
 if [[ $HOSTNAME =~ "leeping" ]] ; then
     export CUDA_HOME=/opt/cuda
@@ -29,13 +29,13 @@ elif [[ $HOSTNAME =~ "certainty" || $HOSTNAME =~ "compute-" || $HOSTNAME =~ "lar
     export PATH=$CUDA_HOME/bin:$PATH
     export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$CUDA_HOME/lib:$LD_LIBRARY_PATH
     export INCLUDE=$CUDA_HOME/include:$INCLUDE
-    export BAK=/scratch/leeping/runcuda-backups
+    # export BAK=/scratch/leeping/runcuda-backups
 elif [[ $HOSTNAME =~ "kid" ]] ; then
     export CUDA_HOME=/sw/keeneland/cuda/4.1/linux_binary
     export PATH=$CUDA_HOME/bin:$PATH
     export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$CUDA_HOME/lib:$LD_LIBRARY_PATH
     export INCLUDE=$CUDA_HOME/include:$INCLUDE
-    export BAK=/lustre/medusa/leeping/runcuda-backups
+    # export BAK=/lustre/medusa/leeping/runcuda-backups
 elif [[ $HOSTNAME =~ "icme-gpu" || $HOSTNAME =~ "node0" ]] ; then
     module load cuda50/toolkit/5.0.35
     export OPENMM_CUDA_COMPILER=/cm/shared/apps/cuda50/toolkit/5.0.35/bin/nvcc
@@ -45,7 +45,7 @@ elif [[ $HOSTNAME =~ "longhorn" ]] ; then
     module load cuda/4.1
     export CUDA_CACHE_PATH=$SCRATCH/.nv/ComputeCache
     export OPENMM_CUDA_COMPILER=/share/apps/cuda/4.1/cuda/bin/nvcc
-    export BAK=$SCRATCH/runcuda-backups
+    # export BAK=$SCRATCH/runcuda-backups
 elif [[ $HOSTNAME =~ "not0rious" ]] ; then
     module load openmm
 elif [[ $HOSTNAME =~ "ls4" ]] ; then
@@ -54,13 +54,13 @@ elif [[ $HOSTNAME =~ "ls4" ]] ; then
     module load cuda/5.0
     export CUDA_CACHE_PATH=$SCRATCH/.nv/ComputeCache
     export OPENMM_CUDA_COMPILER=/opt/apps/cuda/5.0/bin/nvcc
-    export BAK=$SCRATCH/runcuda-backups
+    # export BAK=$SCRATCH/runcuda-backups
 elif [[ $HOSTNAME =~ "cn" ]] ; then
     # HS GPU Cluster
     module load cuda
     export CUDA_CACHE_PATH=/hsgs/projects/pande/leeping/.nv/ComputeCache
     export OPENMM_CUDA_COMPILER=/opt/cuda5.0/bin/nvcc
-    export BAK=/hsgs/projects/pande/leeping/scratch/runcuda-backups
+    # export BAK=/hsgs/projects/pande/leeping/scratch/runcuda-backups
 elif [[ $ARCHIVER == "ranch.tacc.utexas.edu" ]] ; then
     # Currently this is the only way I can be sure I'm on Stampede...
     # This is a wild shot. Kamelasa! It's difficult to tell the cluster name 
@@ -68,7 +68,7 @@ elif [[ $ARCHIVER == "ranch.tacc.utexas.edu" ]] ; then
     module load cuda/5.0
     export CUDA_CACHE_PATH=$SCRATCH/.nv/ComputeCache
     export OPENMM_CUDA_COMPILER=`which nvcc`
-    export BAK=$SCRATCH/runcuda-backups
+    # export BAK=$SCRATCH/runcuda-backups
 fi
 
 echo "#=======================#"
@@ -98,11 +98,12 @@ echo
 echo $@
 
 rm -f npt_result.p npt_result.p.bz2
+export PYTHONUNBUFFERED="y"
 time $@
 # Delete backup files that are older than one week.
-find $BAK/$PWD -type f -mtime +7 -exec rm {} \;
-mkdir -p $BAK/$PWD
-cp * $BAK/$PWD
+# find $BAK/$PWD -type f -mtime +7 -exec rm {} \;
+# mkdir -p $BAK/$PWD
+# cp * $BAK/$PWD
 # For some reason I was still getting error messages about the bzip already existing..
 rm -f npt_result.p.bz2
 bzip2 npt_result.p
