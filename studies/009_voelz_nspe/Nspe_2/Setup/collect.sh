c27e14dddbfc8f7c4a24b1a4783d3f8d63315921
leeping <leeping@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2013-03-27 18:01:55 +0000

    Included targets for tutorials as zip files. Significantly reduced repository size.
    git-svn-id: https://simtk.org/svn/forcebalance@291 e93b7d5b-8bb4-0410-82c6-a20ae092c748

--- studies/009_voelz_nspe/Nspe_2/Setup/collect.sh
+++
@@ -1,9 +0,0 @@
-tar xjf structures.tar.bz2
-for i in `ls structures/xyz/*.xyz | awk -F '.o' '{print $1}' | awk -F 'c.' '{print $NF}' | sort -n | uniq`; do
-    for j in `ls structures/xyz/*.xyz | awk -F 'o.' '{print $NF}' | awk -F '.' '{print $1}' | sort -n | uniq`; do
-	f=structures/xyz/min.c.$i.o.$j.xyz
-	if [ -f $f ]; then
-	    cat $f >> all.xyz
-	fi
-    done
-done
