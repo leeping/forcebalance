f2ca8d0fadc4d3a163c01275f6fa99b9a4a36f64
ahvigil <ahvigil@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2013-06-27 17:49:48 +0000

    Added a basic check for numpy and scipy modules to forcebalance.__init__
    git-svn-id: https://simtk.org/svn/forcebalance@344 e93b7d5b-8bb4-0410-82c6-a20ae092c748

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+try:
+    import numpy
+except ImportError:
+    print "Could not load numpy module, exiting..."
+    exit()
+
+try:
+    import scipy
+except ImportError:
+    print "Could not load scipy module, exiting..."
+    exit()
