d1db08004480405b6412715ab5d9089f45bc6923
ahvigil <ahvigil@e93b7d5b-8bb4-0410-82c6-a20ae092c748>
2013-07-02 00:15:49 +0000

    improved tests for parser and molecule
    git-svn-id: https://simtk.org/svn/forcebalance@362 e93b7d5b-8bb4-0410-82c6-a20ae092c748

--- test/test_molecule.py
+++ test/test_molecule.py
@@ -19,14 +19,14 @@ class TestPDBMolecule(ForceBalanceTestCase):
             self.fail("\nUnable to open pdb file")
 
     def tearDown(self):
-        os.system('rm -rf *.xyz *.gro')
+        os.system('rm -rf {name}.xyz {name}.gro {name}.arc'.format(name=self.source[:-4]))
         super(TestPDBMolecule,self).tearDown()
 
     def test_xyz_conversion(self):
         """Check molecule conversion from pdb to xyz format"""
-        self.molecule.write(self.source[:-3] + '.xyz')
+        self.molecule.write(self.source[:-3] + 'xyz')
         try:
-            molecule1 = forcebalance.molecule.Molecule(self.source[:-3] + '.xyz', build_topology=False)
+            molecule1 = forcebalance.molecule.Molecule(self.source[:-3] + 'xyz', build_topology=False)
         except:
             self.fail("\nConversion to xyz format creates unreadable file")
 
@@ -35,9 +35,9 @@ class TestPDBMolecule(ForceBalanceTestCase):
 
     def test_gro_conversion(self):
         """Check molecule conversion from pdb to gro format"""
-        self.molecule.write(self.source[:-3] + '.gro')
+        self.molecule.write(self.source[:-3] + 'gro')
         try:
-            molecule1 = forcebalance.molecule.Molecule(self.source[:-3] + '.gro', build_topology=False)
+            molecule1 = forcebalance.molecule.Molecule(self.source[:-3] + 'gro', build_topology=False)
         except:
             self.fail("\nConversion to gro format creates unreadable file")
 
@@ -48,10 +48,31 @@ class TestPDBMolecule(ForceBalanceTestCase):
         msg = "\nConversion from pdb to gro yields different xyz coordinates\npdb:\n%s\n\ngro:\n%s\n" %\
         (str(self.molecule.Data['xyzs'][0]), str(molecule1.Data['xyzs'][0])))
 
+    def test_arc_conversion(self):
+        """Check molecule conversion from pdb to arc format"""
+        self.molecule.Data['tinkersuf']=['']*len(self.molecule.Data['resname'])  # suppress topology warning
+        self.molecule.write(self.source[:-3] + 'arc')
+        try:
+            molecule1 = forcebalance.molecule.Molecule(self.source[:-3] + 'arc',build_topology=False)
+        except:
+            self.fail("\nConversion to arc (TINKER) format creates unreadable file")
 
+        self.assertEqual(len(self.molecule.Data['resid']), len(molecule1.Data['resid']),
+                        msg = "\nConversion from pdb to arc (TINKER) yields different number of residues")
 
+        self.assertTrue((abs(self.molecule.Data['xyzs'][0]-molecule1.Data['xyzs'][0])<.0000001).all(),
+        msg = "\nConversion from pdb to arc yields different xyz coordinates\npdb:\n%s\n\narc:\n%s\n" %\
+        (str(self.molecule.Data['xyzs'][0]), str(molecule1.Data['xyzs'][0])))
 
+    def test_pdb_topology_build(self):
+        """Check reading pdb with build_topology=True"""
+        try:
+            molecule = forcebalance.molecule.Molecule(self.source, build_topology=True)
+        except:
+            self.fail("\nFailed to load pdb with build_topology=True")
 
+        self.assertEqual(len(self.molecule.Data['resid']), len(molecule.Data['resid']),
+                        msg = "\nTopology build yields different number of residues")
 
 if __name__ == '__main__':
     unittest.main()
