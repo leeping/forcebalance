6e970fa78162a6898f2a015eab748db284da0f0b
Lee-Ping Wang <leeping@stanford.edu>
2013-07-09 21:50:25 -0700

    Updated documentation

---
+++ doc/upload-documentation.sh
@@ -0,0 +1,4 @@
+# Prior to this, run makedocumentation.py
+rsync -auvz --delete html/ /home/leeping/Dropbox/Public/ForceBalance_Doc/
+cp ForceBalance-Manual.pdf /home/leeping/Dropbox/Public/ForceBalance_Doc/ForceBalance-Manual.pdf
+cp ForceBalance-API.pdf /home/leeping/Dropbox/Public/ForceBalance_Doc/ForceBalance-API.pdf
