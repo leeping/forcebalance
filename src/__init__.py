cf542e85db6fe3529ed9487c0b3ab2c04f5a4878
ahvigil <ahvigil@mail.sfsu.edu>
2013-07-16 10:43:07 -0700

    logging support in forcebalance

--- src/__init__.py
+++ src/__init__.py
@@ -10,4 +10,11 @@ except ImportError:
     print "Could not load scipy module, exiting..."
     exit()
 
-import parser, forcefield, optimizer, objective
+import logging
+import parser, forcefield, optimizer, objective, nifty
+
+# set up package level logger that by default writes to stdout
+logging.logger=logging.getLogger('forcebalance')
+logging.logger.addHandler(nifty.RawFileHandler('test.log','w'))
+logging.logger.setLevel(logging.INFO)
+
