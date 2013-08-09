85d8c76ee54d683714dfcb049da6c4329755e4c2
ahvigil <ahvigil@mail.sfsu.edu>
2013-08-09 11:14:51 -0700

    Add default handler to package logger

--- test/__init__.py
+++ test/__init__.py
@@ -149,6 +149,7 @@ class ForceBalanceTestRunner(object):
        in the standard unittest.TestResult format"""
     def __init__(self, logger=forcebalance.output.getLogger("forcebalance.test"), verbose = False):
         self.logger = logger
+        forcebalance.output.getLogger("forcebalance.test")
         
     def check(self, test_modules=__all__):
         """This tries importing test modules which is helpful for error checking
@@ -193,6 +194,8 @@ class ForceBalanceTestRunner(object):
         tests.addTests(systemTests) # integration tests should be run after other tests
 
         result = ForceBalanceTestResult()
+        
+        forcebalance.output.getLogger("forcebalance").addHandler(forcebalance.output.NullHandler())
 
         ### START TESTING ###
         # run any pretest tasks before first test
