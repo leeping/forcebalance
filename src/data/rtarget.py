f29e6c9a833a75f04204c528a65095b4271c4afd
ahvigil <ahvigil@mail.sfsu.edu>
2013-08-22 15:27:43 -0700

    Tweaks to get remote task running

--- src/data/rtarget.py
+++ src/data/rtarget.py
@@ -3,6 +3,11 @@ import forcebalance.objective
 import forcebalance.nifty
 import tarfile
 import os
+import forcebalance.output
+logger = forcebalance.output.getLogger("forcebalance")
+logger.setLevel(forcebalance.output.DEBUG)
+
+os.system('rm -rf ../cache')
 
 # load pickled variables from forcebalance.p
 f=open('forcebalance.p', 'r')
@@ -12,22 +17,35 @@ f.close()
 options['root'] = os.getcwd()
 
 # set up forcefield
+tar = tarfile.open("forcefield.tar.bz2", "r")
+tar.extractall()
+tar.close()
 forcefield.make(mvals)
 
 # set up and evaluate target
 tar = tarfile.open("target.tar.bz2", "r")
-os.mkdir("targets")
-tar.extractall(path="targets")
+tar.extractall()
 tar.close()
 
 Tgt = forcebalance.objective.Implemented_Targets[tgt_opts['type']](options,tgt_opts,forcefield)
-Tgt.stage(mvals, AGrad = True, AHess = True)
+Tgt.submit_jobs(mvals, AGrad = True, AHess = True)
+
+os.chdir("temp/cluster-12/")
+
 Ans = Tgt.get(mvals, AGrad=True, AHess=True)
 
+os.chdir("../..")
+
 with open('objective.p', 'w') as f:
-    forcebalance.nifty.lp_dump(f,Ans)        # or some other method of storing resulting objective
+    forcebalance.nifty.lp_dump(Ans, f)        # or some other method of storing resulting objective
 
 # also run target.indicate()
 logger = forcebalance.output.getLogger("forcebalance")
-logger.addHandler(forcebalance.nifty.RawFileHandler("indicate.log"))
+logger.addHandler(forcebalance.output.RawFileHandler("indicate.log"))
 Tgt.indicate()
+
+os.chdir(Tgt.root)
+os.system('rm -rf temp forcefield targets backups')
+
+print "\n"
+    
