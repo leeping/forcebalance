004c9fdc156a77292ea95c02b3a35937f2b3f965
Sara Kokkila <skokkila@fire-4-0-ext.slac.stanford.edu>
2013-08-30 15:45:28 -0700

    Made sure that Sara was running the correct version of Python. :D

--- src/data/run_psi_rdvr3_objective.sh
+++ src/data/run_psi_rdvr3_objective.sh
@@ -3,6 +3,8 @@
 . /etc/profile
 . $HOME/.bashrc
 
+export PATH=/opt/python/2.7.2/bin:$PATH
+export LD_LIBRARY_PATH=/opt/python/2.7.2/lib:$LD_LIBRARY_PATH
 module load psi/tip.opt
 
 echo "#=======================#"
