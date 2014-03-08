85e542765c0d78c477462187352ce7b154e70051
kmckiern <kmckiern@stanford.edu>
2014-03-07 20:12:43 -0800

    Remove print statements

--- src/gmxio.py
+++ src/gmxio.py
@@ -1408,25 +1408,14 @@ class Lipid_GMX(Lipid):
                 # p_u assumes that all phasepoint keys are of the same unit.  This may be a reasonable assumption.
 		p_u = self.PhasePoints[0][2]
                 td_vals = (temperature, pressure, p_u)
-		print '**0', self.lipid_mols
                 if not td_vals in self.lipid_mols:
-		    print '**1', self.lipid_mols
-		    print '**1 new', self.lipid_mols_new
-		    print '**11', td_vals, os.getcwd()
                     self.lipid_mols_new[td_vals] = [Molecule(os.path.join(os.getcwd(),'lipid-md.gro'))]
                 else:
                     if GoodStep() and len(self.lipid_mols[td_vals]) == int(self.RefData['n_ic'][td_vals]):
-		        print '**2', self.lipid_mols
-		        print '**2 new', self.lipid_mols_new
-		    	print '**22', td_vals, os.getcwd()
                         if td_vals in self.lipid_mols_new:
                             self.lipid_mols[td_vals] = self.lipid_mols_new[td_vals]
                         super(Lipid_GMX, self).npt_simulation(temperature, pressure, simnum)
-		    	print 'will this work?', self.lipid_mols
                     else:
-			print '**3', self.lipid_mols
-		        print '**3 new', self.lipid_mols_new
-			print '**33', td_vals, os.getcwd()
                         self.lipid_mols_new[td_vals].append(Molecule(os.path.join(os.getcwd(),'lipid-md.gro')))
             else:
                 if GoodStep() and (temperature, pressure) in self.LfDict_New:
