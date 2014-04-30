3748aca4042578691dcbefcaa0bc1bd425737682
Lee-Ping <leeping@stanford.edu>
2014-04-29 22:28:16 -0700

    Fix an issue with writing Q-Chem input

--- src/molecule.py
+++ src/molecule.py
@@ -2690,11 +2690,11 @@ class Molecule(object):
                     if SectName == 'rem':
                         for key, val in self.qcrems[remidx].items():
                             out.append("%-21s %-s" % (key, str(val)))
-                        remidx += 1
                     if SectName == 'comments' and 'comms' in self.Data:
                         out.append(self.comms[I])
                     out.append('$end')
                 else:
+                    remidx += 1
                     out.append('@@@@')
                 out.append('')
             #if I < (len(self) - 1):
