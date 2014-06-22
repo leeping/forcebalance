1c7d7686d2b5a66904c50561ba92e80f8034d5a3
Joe Napoli <jnapoli@markland.cm.cluster>
2014-06-22 11:23:25 -0700

    Remove unnecessary code in test script. Add directory containing qtip4pf xml and pdb files.

--- test/test_rpmd.py
+++ test/test_rpmd.py
@@ -11,13 +11,13 @@ from forcebalance.openmmio import OpenMM
 # This script creates an OpenMM Engine object and runs a short RPMD simulation.
 try:
     import simtk.openmm
-    print('\nOpenMM successfully imported\n')
+    print('OpenMM successfully imported\n')
 except: 
-    print('\nOpenMM unsuccessfully imported\n')
+    print('OpenMM unsuccessfully imported\n')
 
 os.chdir(os.path.join('test','files','rpmd_files'))
 
-ommEngine = OpenMM(ffxml='qtip4pf.xml', pdb='h2o.pdb', platname='CUDA', precision='mixed', rpmd='True')
+ommEngine = OpenMM(ffxml='qtip4pf.xml', pdb='h2o.pdb', platname='CUDA', precision='mixed')
 
 print('Engine successfully created\n')
 
@@ -30,18 +30,10 @@ os.chdir('temp')
 print('Creating simulation...\n')
 
 # Run RPMD and save trajectory data
-#ommEngine.prepare()
-#ommEngine.update_simulation()
 ommEngine.create_simulation(timestep=0.5, temperature=300, rpmd_opts=['32'])
 
 print('Starting MD...\n')
 
-out_file=open('data_output.txt','w')
-
 data = ommEngine.molecular_dynamics(nsteps=10000, nsave=100, timestep=0.5, temperature=300, verbose=True, save_traj=True)
 
-for elem in data:
-    out_file.write(elem)
-
 sys.exit('Finished simulation!')
-
