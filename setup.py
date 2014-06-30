63a94ad21fbbe6143b941a7a807ba661aa683d65
Lee-Ping <leeping@stanford.edu>
2014-06-30 10:18:57 -0700

    Update version number (also for purposes of testing)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 #| doc/api_header.tex              |#
 #| src/__init__.py                 |#
 #===================================#
-__version__ = "v1.3.0"
+__version__ = "v1.3.1"
 try:
     # use git to find current version
     git_describe = subprocess.check_output(["git", "describe"]).strip()
