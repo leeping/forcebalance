1f600178c980a3311d54af043dc97b267f5e8d32
kmckiern <kmckiern@stanford.edu>
2014-09-05 13:32:02 -0700

    Sort of updating

--- src/parser.py
+++ src/parser.py
@@ -220,7 +220,7 @@ tgt_opts_types = {
                  "w_cp"         : (1.0, 0, 'Weight of isobaric heat capacity', 'Condensed phase property targets', 'liquid, lipid'),
                  "w_eps0"       : (1.0, 0, 'Weight of dielectric constant', 'Condensed phase property targets', 'liquid, lipid'),
                  "w_al"         : (1.0, 0, 'Weight of average area per lipid', 'Lipid property targets', 'lipid'),
-                 "w_scd"        : (1.0, 0, 'Weight of dielectric constant', 'Lipid property targets', 'lipid'),
+                 "w_scd"        : (1.0, 0, 'Weight of deuterium order parameter', 'Lipid property targets', 'lipid'),
                  "w_energy"     : (1.0, 0, 'Weight of energy', 'Ab initio targets', 'liquid'),
                  "w_force"      : (1.0, 0, 'Weight of atomistic forces', 'Ab initio targets', 'liquid'),
                  "w_netforce"   : (0.0, 0, 'Weight of net forces (condensed to molecules, residues, or charge groups)', 'Ab initio targets', 'abinitio'),
