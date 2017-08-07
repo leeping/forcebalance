d594b11fd78a73af7bc4b1df26b08465c596b23c
qyd <saviorbruce@gmail.com>
2017-08-07 15:06:10 -0700

    fix bugs when reading qchem input files with uppercase key like $Rem and output files with no input block printed

--- src/molecule.py
+++ src/molecule.py
@@ -820,7 +820,7 @@ def arc(Mol, begin=None, end=None, RMSD=True, align=True):
     Arc : np.ndarray
         Arc length between frames in Angstrom, length is n_frames - 1
     """
-    if align: 
+    if align:
         Mol.align()
     if begin is None:
         begin = 0
@@ -1682,7 +1682,7 @@ class Molecule(object):
         else:
             for i in range(len(self)):
                 self.xyzs[i] -= self.xyzs[i].mean(0)
-            
+
     def build_bonds(self):
         """ Build the bond connectivity graph. """
         sn = self.top_settings['topframe']
@@ -2122,13 +2122,13 @@ class Molecule(object):
         Recursive function that orders atoms based on connectivity.
         To call the function, pass in the topology object (e.g. M.molecules[0])
         and the index of the atom assigned to be "first".
-        
+
         The neighbors of "i" are placed in decreasing order of the
         maximum length of the shortest path to all other atoms - i.e.
         an atom closer to the "edge" of the molecule has a longer
         shortest-path to atoms on the other "edge", and they should
         be added first.
-        
+
         Snippet:
 
         from forcebalance.molecule import *
@@ -2145,7 +2145,7 @@ class Molecule(object):
         # Write the new Molecule object
         newM = M.atom_select(newOrder)
         newM.write('reordered1.xyz')
-        
+
         Parameters
         ----------
         m : topology object (contained in Molecule)
@@ -2155,11 +2155,11 @@ class Molecule(object):
         currList : list
             Current list of new atom orderings
         max_min_path : dict
-            Dictionary mapping 
+            Dictionary mapping
         """
         if m not in self.molecules:
             raise RuntimeError("topology is not part of Molecule object")
-        if max_min_path is None: 
+        if max_min_path is None:
             spl = nx.shortest_path_length(m)
             # Dictionary mapping atom index to
             # maximum length of shortest path to all other atoms
@@ -2224,7 +2224,7 @@ class Molecule(object):
             xyzi = self.xyzs[i].copy()
             j=i+1
             xyzj = self.xyzs[j].copy()
-            if align: 
+            if align:
                 xyzi -= xyzi.mean(0)
                 xyzj -= xyzj.mean(0)
                 tr, rt = get_rotate_translate(xyzj, xyzi)
@@ -2895,7 +2895,7 @@ class Molecule(object):
             if "Z-matrix Print" in line:
                 zmatrix = True
             if re.match('^\$',line):
-                wrd = re.sub('\$','',line)
+                wrd = re.sub('\$','',line).lower()
                 if zmatrix:
                     if wrd == 'end':
                         zmatrix = False
@@ -3390,7 +3390,7 @@ class Molecule(object):
         elif len(Floats['energy_mp2']) > 0:
             Answer['qm_energies'] = Floats['energy_mp2']
         elif len(energy_scf) > 0:
-            if 'correlation' in Answer['qcrems'][0] and Answer['qcrems'][0]['correlation'].lower() in ['mp2', 'rimp2', 'ccsd', 'ccsd(t)']:
+            if len(Answer['qcrems']) > 0 and 'correlation' in Answer['qcrems'][0] and Answer['qcrems'][0]['correlation'].lower() in ['mp2', 'rimp2', 'ccsd', 'ccsd(t)']:
                 logger.error("Q-Chem was called with a post-HF theory but we only got the SCF energy\n")
                 raise RuntimeError
             Answer['qm_energies'] = energy_scf
