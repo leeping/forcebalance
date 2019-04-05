7f191d0721b9d23320d3cb641d0199173afc2478
qyd <saviorbruce@gmail.com>
2019-04-05 14:26:10 -0700

    convert test_liquid.py to pytest

--- src/tests/test_liquid.py
+++ src/tests/test_liquid.py
@@ -1,55 +1,61 @@
 from __future__ import absolute_import
-from __init__ import ForceBalanceTestCase
-import unittest
+import os
+import sys
+import shutil
 import forcebalance
-import os, sys
 from forcebalance.parser import parse_inputs
 from forcebalance.forcefield import FF
 from forcebalance.objective import Objective
 from forcebalance.optimizer import Optimizer
 from forcebalance import optimizer as fbopt
 
-class TestWaterTutorial(ForceBalanceTestCase):
-    def setUp(self):
-        super(ForceBalanceTestCase,self).setUp()
-        os.system('rm -rf test/files/test_liquid.run')
-        os.system('cp -r test/files/test_liquid/ test/files/test_liquid.run/')
-        os.chdir('test/files/test_liquid.run')
+class TestWaterTutorial:
+    @classmethod
+    def setup_class(cls):
+        cls.cwd = os.path.dirname(os.path.realpath(__file__))
+        # copy folder 'files/test_liquid' into a new folder 'files/test_liquid.run'
+        os.chdir(os.path.join(cls.cwd, 'files'))
+        tmpfolder = os.path.join(cls.cwd, 'files', 'test_liquid.run')
+        source_folder = os.path.join(cls.cwd, 'files', 'test_liquid')
+        if os.path.isdir(tmpfolder):
+            shutil.rmtree(tmpfolder)
+        shutil.copytree(source_folder, tmpfolder)
+        os.chdir(tmpfolder)
 
-    def tearDown(self):
-        os.chdir('..')
-        os.system('rm -rf test_liquid.run')
-        super(ForceBalanceTestCase,self).tearDown()
+    @classmethod
+    def tearDown(cls):
+        # remove temporary folder 'files/test_liquid.run'
+        tmpfolder = os.path.join(cls.cwd, 'files', 'test_liquid.run')
+        if os.path.isdir(tmpfolder):
+            shutil.rmtree(tmpfolder)
 
-    def runTest(self):
+    def test_liquid(self):
         """Check liquid target with existing simulation data"""
-        if not sys.version_info <= (2,7):
-            self.skipTest("Existing pickle file only works with Python 3")
-        
-        self.logger.debug("\nSetting input file to 'single.in'\n")
+        # if not sys.version_info <= (2,7):
+        #     self.skipTest("Existing pickle file only works with Python 3")
+
+        print("Setting input file to 'single.in'")
         input_file='single.in'
 
         ## The general options and target options that come from parsing the input file
-        self.logger.debug("Parsing inputs...\n")
+        print("Parsing inputs...")
         options, tgt_opts = parse_inputs(input_file)
-        self.logger.debug("options:\n%s\n\ntgt_opts:\n%s\n\n" % (str(options), str(tgt_opts)))
+        print("options:\n%s\n\ntgt_opts:\n%s\n\n" % (str(options), str(tgt_opts)))
 
         forcefield  = FF(options)
         objective   = Objective(options, tgt_opts, forcefield)
         ## The optimizer component of the project
-        self.logger.debug("Creating optimizer: ")
+        print("Creating optimizer: ")
         optimizer   = Optimizer(options, objective, forcefield)
-        self.assertEqual(Optimizer, type(optimizer), msg="\nExpected forcebalance optimizer object")
-        self.logger.debug(str(optimizer) + "\n")
+        assert isinstance(optimizer, Optimizer), "Expected forcebalance optimizer object"
+        print(str(optimizer))
 
         ## Actually run the optimizer.
-        self.logger.debug("Done setting up! Running optimizer...\n")
+        print("Done setting up! Running optimizer...")
         result = optimizer.Run()
-        self.logger.debug("\nOptimizer finished. Final results:\n")
-        self.logger.debug(str(result) + '\n')
+        print("\nOptimizer finished. Final results:")
+        print(str(result))
 
         liquid_obj_value = optimizer.Objective.ObjDict['Liquid']['x']
-        self.assertTrue(liquid_obj_value < 20, msg="\nLiquid objective function should give < 20 (about 17.23) total value.")
+        assert liquid_obj_value < 20, "Liquid objective function should give < 20 (about 17.23) total value."
 
-if __name__ == '__main__':
-    unittest.main()
