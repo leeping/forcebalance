2bf1a120e47714dc64cc433ff84dfb863a8cc625
qyd <saviorbruce@gmail.com>
2019-07-01 08:21:52 -0700

    fix small bug in smirnoffio.py

--- src/smirnoffio.py
+++ src/smirnoffio.py
@@ -255,7 +255,7 @@ class SMIRNOFF(OpenMM):
                 if self.mol.boxes[I].alpha != 90.0 or self.mol.boxes[I].beta != 90.0 or self.mol.boxes[I].gamma != 90.0:
                     logger.error('OpenMM cannot handle nonorthogonal boxes.\n')
                     raise RuntimeError
-                box_omm = np.diagonal([self.mol.boxes[I].a, self.mol.boxes[I].b, self.mol.boxes[I].c]) * angstrom
+                box_omm = np.diag([self.mol.boxes[I].a, self.mol.boxes[I].b, self.mol.boxes[I].c]) * angstrom
             else:
                 box_omm = None
             # Finally append it to list.
