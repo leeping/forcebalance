f50d14e859cccb5f02f439e79bc6a07580c0f788
hyesu jang <hyesu@hyesus-MacBook-Pro.local>
2019-10-23 13:10:41 -0700

    convert to pytest format

--- src/tests/test_optimizer.py
+++ src/tests/test_optimizer.py
@@ -1,42 +1,66 @@
 from __future__ import absolute_import
-from __init__ import ForceBalanceTestCase
-import unittest
+# from __init__ import ForceBalanceTestCase
+# import unittest
 import numpy
 import forcebalance
-import os
+import os, sys
 import tarfile
 import logging
 
 logger = logging.getLogger("test")
 
-class TestOptimizer(ForceBalanceTestCase):
-    def setUp(self):
-        super(ForceBalanceTestCase,self).setUp()
-        os.chdir('studies/001_water_tutorial')
-        self.input_file='very_simple.in'
+# class TestOptimizer(ForceBalanceTestCase):
+class TestOptimizer:
+    # def setUp(self):
+        # super(ForceBalanceTestCase,self).setUp()
+    @classmethod
+    def setup_class(cls):
+        cls.cwd = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(os.path.join(cls.cwd, '../../studies/001_water_tutorial'))
+        # input_file='very_simple.in'
+        # targets = tarfile.open('targets.tar.bz2','r')
+        # targets.extractall()
+        # targets.close()
+        #
+        # options, tgt_opts = forcebalance.parser.parse_inputs(input_file)
+        #
+        # options.update({'writechk':'checkfile.tmp'})
+        #
+        # forcefield  = forcebalance.forcefield.FF(options)
+        # objective   = forcebalance.objective.Objective(options, tgt_opts, forcefield)
+        # try: optimizer   = forcebalance.optimizer.Optimizer(options, objective, forcefield)
+        # except: fail("\nCouldn't create optimizer")
+
+    # def tearDown(self):
+    @classmethod
+    def teardown_class(cls):
+        os.system('rm -rf result *.bak *.tmp')
+        # super(ForceBalanceTestCase,self).tearDown()
+
+    # def runTest(self):
+    def test_optimizer(self):
+        # moved from setUp
+        input_file='very_simple.in'
         targets = tarfile.open('targets.tar.bz2','r')
         targets.extractall()
         targets.close()
 
-        self.options, self.tgt_opts = forcebalance.parser.parse_inputs(self.input_file)
+        options, tgt_opts = forcebalance.parser.parse_inputs(input_file)
 
-        self.options.update({'writechk':'checkfile.tmp'})
+        options.update({'writechk':'checkfile.tmp'})
 
-        self.forcefield  = forcebalance.forcefield.FF(self.options)
-        self.objective   = forcebalance.objective.Objective(self.options, self.tgt_opts, self.forcefield)
-        try: self.optimizer   = forcebalance.optimizer.Optimizer(self.options, self.objective, self.forcefield)
-        except: self.fail("\nCouldn't create optimizer")
+        forcefield  = forcebalance.forcefield.FF(options)
+        objective   = forcebalance.objective.Objective(options, tgt_opts, forcefield)
+        try: optimizer   = forcebalance.optimizer.Optimizer(options, objective, forcefield)
+        except: fail("\nCouldn't create optimizer")
 
-    def tearDown(self):
-        os.system('rm -rf result *.bak *.tmp')
-        super(ForceBalanceTestCase,self).tearDown()
+        optimizer.writechk()
+        # self.assertTrue(os.path.isfile(options['writechk']),
+        # msg="\nOptimizer.writechk() didn't create expected file at %s" % options['writechk'])
+        assert os.path.isfile(options['writechk']), "Optimizer.writechk() didn't create expected file at %s " % options['writechk']
+        read = optimizer.readchk()
+        # self.assertEqual(type(read), dict)
+        assert isinstance(read, dict)
 
-    def runTest(self):
-        self.optimizer.writechk()
-        self.assertTrue(os.path.isfile(self.options['writechk']),
-        msg="\nOptimizer.writechk() didn't create expected file at %s" % self.options['writechk'])
-        read = self.optimizer.readchk()
-        self.assertEqual(type(read), dict)
-
-if __name__ == '__main__':           
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
