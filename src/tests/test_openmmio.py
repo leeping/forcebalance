ecc453252ba7b4b702ec8dc251c3fea8dc500056
Jeff Wagner <jwagnerjpl@gmail.com>
2019-10-25 19:57:57 -0700

    refactor test_openmmio to use pytest

--- src/tests/test_openmmio.py
+++ src/tests/test_openmmio.py
@@ -11,27 +11,26 @@ import pytest
 # class TestLiquid_OpenMM(ForceBalanceTestCase, TargetTests):
 class TestLiquid_OpenMM(TargetTests):
     # def setUp(self):
-    @classmethod
-    def setup_class(cls):
+    def setup_method(self, method):
         # super(TestLiquid_OpenMM, cls).setup_class()
         # self.skipTest("Needs optimizing to reduce runtime")
-        TargetTests.setup_class.im_func()
+        super().setup_method(method)# .im_func()
         # settings specific to this target
-        cls.options.update({
+        self.options.update({
                 'jobtype': 'NEWTON',
                 'forcefield': ['dms.xml']})
 
-        cls.tgt_opt.update({'type':'LIQUID_OPENMM',
+        self.tgt_opt.update({'type':'LIQUID_OPENMM',
             'name':'dms-liquid'})
 
-        cls.ff = forcebalance.forcefield.FF(cls.options)
+        self.ff = forcebalance.forcefield.FF(self.options)
 
-        cls.ffname = cls.options['forcefield'][0][:-3]
-        cls.filetype = cls.options['forcefield'][0][-3:]
-        cls.mvals = [.5]*cls.ff.np
+        self.ffname = self.options['forcefield'][0][:-3]
+        self.filetype = self.options['forcefield'][0][-3:]
+        self.mvals = [.5]*self.ff.np
 
-        cls.target = forcebalance.openmmio.Liquid_OpenMM(cls.options, cls.tgt_opt, cls.ff)
-        cls.target.stage(cls.mvals)
+        self.target = forcebalance.openmmio.Liquid_OpenMM(self.options, self.tgt_opt, self.ff)
+        self.target.stage(self.mvals)
         pytest.addCleanup(os.system, 'rm -rf temp')
 
     def shortDescription(self):
@@ -41,32 +40,32 @@ class TestLiquid_OpenMM(TargetTests):
 # class TestInteraction_OpenMM(ForceBalanceTestCase, TargetTests):
 class TestInteraction_OpenMM(TargetTests):
     # def setUp(self):
-    @classmethod
-    def setup_class(cls):
-        TargetTests.setup_class.im_func()
+
+    def setup_method(self, method):
+        super().setup_method(method)
         # TargetTests.setup_class(cls)
         # settings specific to this target
-        cls.options.update({
+        self.options.update({
                 'jobtype': 'NEWTON',
                 'forcefield': ['dms.xml']})
 
-        cls.tgt_opt.update({"type" : "Interaction_OpenMM",
+        self.tgt_opt.update({"type" : "Interaction_OpenMM",
                             "name" : "S2EPose",
                             "fragment1" : "1-9",
                             "fragment2" : "10-18"})
 
-        cls.ff = forcebalance.forcefield.FF(cls.options)
+        self.ff = forcebalance.forcefield.FF(self.options)
 
-        cls.ffname = cls.options['forcefield'][0][:-3]
-        cls.filetype = cls.options['forcefield'][0][-3:]
-        cls.mvals = [.5]*cls.ff.np
+        self.ffname = self.options['forcefield'][0][:-3]
+        self.filetype = self.options['forcefield'][0][-3:]
+        self.mvals = [.5]*self.ff.np
 
-        cls.target = forcebalance.openmmio.Interaction_OpenMM(cls.options, cls.tgt_opt, cls.ff)
-        pytest.addCleanup(os.system, 'rm -rf temp')
+        self.target = forcebalance.openmmio.Interaction_OpenMM(self.options, self.tgt_opt, self.ff)
+    def teardown_method(self):
+        os.system('rm -rf temp')
+        super().teardown_method()
+
 
     def shortDescription(self):
         """@override ForceBalanceTestCase.shortDescription()"""
         return super(TestInteraction_OpenMM,self).shortDescription() + " (Interaction_OpenMM)"
-
-# if __name__ == '__main__':
-#     unittest.main()
