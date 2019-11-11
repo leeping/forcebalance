leeping/forcebalance

commit 84081d583b0a059e1bc573f1e869f4343b716e88

fix property_estimator import errors

src/property_estimator_io.py
@@ -16,7 +16,6 @@ import numpy as np
 from forcebalance.nifty import warn_once, printcool, printcool_dictionary
 from forcebalance.output import getLogger
 from forcebalance.target import Target
-from propertyestimator.properties import ParameterGradientKey
 
 logger = getLogger(__name__)
 
@@ -29,6 +28,7 @@ try:
     from propertyestimator.utils.openmm import openmm_quantity_to_pint
     from propertyestimator.utils.serialization import TypedJSONDecoder, TypedJSONEncoder
     from propertyestimator.workflow import WorkflowOptions
+    from propertyestimator.properties import ParameterGradientKey
 except ImportError:
     warn_once("Failed to import the propertyestimator package.")
 
@@ -143,11 +143,13 @@ class PropertyEstimate_SMIRNOFF(Target):
             return value
 
     # A dictionary of the units that force balance expects each property in.
-    default_units = {
-        'Density': unit.kilogram / unit.meter ** 3,
-        'DielectricConstant': unit.dimensionless,
-        'EnthalpyOfVaporization': unit.kilojoules / unit.mole
-    }
+    @property
+    def default_units(self):
+        return {
+            'Density': unit.kilogram / unit.meter ** 3,
+            'DielectricConstant': unit.dimensionless,
+            'EnthalpyOfVaporization': unit.kilojoules / unit.mole
+        }
 
     def __init__(self, options, tgt_opts, forcefield):
 
@@ -178,8 +180,7 @@ class PropertyEstimate_SMIRNOFF(Target):
         # Initialize the target.
         self._initialize()
 
-    @staticmethod
-    def _refactor_properties_dictionary(properties):
+    def _refactor_properties_dictionary(self, properties):
         """Refactors a property dictionary of the form
         `property = dict[substance_id][property_index]` to one of the form
         `property = dict[property_type][substance_id][state_tuple]['value' or 'uncertainty']` where
@@ -209,7 +210,7 @@ class PropertyEstimate_SMIRNOFF(Target):
 
                 state_tuple = (f'{temperature:.6f}', f'{pressure:.6f}')
 
-                default_unit = PropertyEstimate_SMIRNOFF.default_units[class_name]
+                default_unit = self.default_units[class_name]
 
                 value = physical_property.value.to(default_unit).magnitude
                 uncertainty = physical_property.uncertainty.to(default_unit).magnitude

src/objective.py
@@ -43,6 +43,12 @@ except:
     logger.warning("SMIRNOFF module import failed; check SMIRNOFF package\n")
 
 try:
+    from forcebalance.property_estimator_io import PropertyEstimate_SMIRNOFF
+except:
+    logger.warning(traceback.format_exc())
+    logger.warning("property estimator module import failed\n")
+
+try:
     from forcebalance.abinitio_internal import AbInitio_Internal
 except:
     logger.warning(traceback.format_exc())
@@ -112,18 +118,9 @@ Implemented_Targets = {
     'OPTGEOTARGET_SMIRNOFF': OptGeoTarget_SMIRNOFF,     # Keeping these two for compatibility with released FB calculation files.
     'TORSIONPROFILE_OPENMM': TorsionProfileTarget_OpenMM,
     'TORSIONPROFILE_SMIRNOFF': TorsionProfileTarget_SMIRNOFF,
+    'PROPERTYESTIMATE_SMIRNOFF': PropertyEstimate_SMIRNOFF,
     'REMOTE_TARGET':RemoteTarget,
     }
-
-## Optional targets
-try:
-    from forcebalance.property_estimator_io import PropertyEstimate_SMIRNOFF
-    Implemented_Targets['PROPERTYESTIMATE_SMIRNOFF'] = PropertyEstimate_SMIRNOFF
-except:
-    logger.warning(traceback.format_exc())
-    logger.warning("property estimator module import failed\n")
-
-
 
 
 ## This is the canonical lettering that corresponds to : objective function, gradient, Hessian.

commit 59eb05a9c7ef58c6515673baba7b330240d9a25d

Fix for bug introduced in 84081d5. Minor linting.

src/property_estimator_io.py
@@ -208,7 +208,7 @@ class PropertyEstimate_SMIRNOFF(Target):
                 temperature = physical_property.thermodynamic_state.temperature.to(unit.kelvin).magnitude
                 pressure = physical_property.thermodynamic_state.pressure.to(unit.atmosphere).magnitude
 
-                state_tuple = ('%.6f'%temperature, '%.6f'%pressure)
+                state_tuple = ('%.6f' % temperature, '%.6f' % pressure)
 
                 default_unit = self.default_units[class_name]
 
@@ -478,7 +478,9 @@ class PropertyEstimate_SMIRNOFF(Target):
                                                                        options=self._options.estimation_options,
                                                                        parameter_gradient_keys=parameter_gradient_keys)
 
-        logger.info('Requesting the estimation of %d properties, and their gradients with respect to %d parameters.\n'%(self._data_set.number_of_properties, len(parameter_gradient_keys)))
+        logger.info('Requesting the estimation of {} properties, and their '
+                    'gradients with respect to {} parameters.\n'.format(self._data_set.number_of_properties,
+                                                                        len(parameter_gradient_keys)))
 
         self._pending_estimate_request.results(True)
 
@@ -505,18 +507,18 @@ class PropertyEstimate_SMIRNOFF(Target):
 
         if len(results.unsuccessful_properties) > 0:
 
-            exceptions = '\n'.join('%s: %s'%(result.directory, result.message) for result in results.exceptions)
+            exceptions = '\n'.join('%s: %s' % (result.directory, result.message) for result in results.exceptions)
 
-            raise ValueError('Some properties could not be estimated:\n\n%s.'%exceptions)
+            raise ValueError('Some properties could not be estimated:\n\n%s.' % exceptions)
 
         elif len(results.exceptions) > 0:
 
-            exceptions = '\n'.join('%s: %s'%(result.directory, result.message) for result in results.exceptions)
+            exceptions = '\n'.join('%s: %s' % (result.directory, result.message) for result in results.exceptions)
 
             # In some cases, an exception will be raised when executing a property but
             # it will not stop the property from being estimated (e.g an error occured
             # while reweighting so a simulation was used to estimate the property instead).
-            logger.warning('A number of non-fatal exceptions occured:\n\n%s.'%exceptions)
+            logger.warning('A number of non-fatal exceptions occured:\n\n%s.' % exceptions)
 
     def _extract_property_data(self, estimation_request, mvals, AGrad):
         """Extract the property estimates #and their gradients#
@@ -542,7 +544,7 @@ class PropertyEstimate_SMIRNOFF(Target):
         # Extract the results from the request.
         results = estimation_request.results()
 
-        estimated_data = PropertyEstimate_SMIRNOFF._refactor_properties_dictionary(results.estimated_properties)
+        estimated_data = self._refactor_properties_dictionary(results.estimated_properties)
         estimated_gradients = {}
 
         if AGrad is False:
@@ -570,7 +572,7 @@ class PropertyEstimate_SMIRNOFF(Target):
                 temperature = physical_property.thermodynamic_state.temperature.to(unit.kelvin).magnitude
                 pressure = physical_property.thermodynamic_state.pressure.to(unit.atmosphere).magnitude
 
-                state_tuple = ('%.6f'%temperature, '%.6f'%pressure)
+                state_tuple = ('%.6f' % temperature, '%.6f' % pressure)
 
                 if state_tuple not in estimated_gradients[class_name][substance_id]:
 
@@ -582,7 +584,7 @@ class PropertyEstimate_SMIRNOFF(Target):
                     parameter_index = self._gradient_key_mappings[gradient.key]
                     gradient_unit = self.default_units[class_name] / self._parameter_units[gradient.key]
 
-                    logger.info('Gradient Value: %f Expected Unit: %s\n'%(gradient.value, gradient_unit))
+                    logger.info('Gradient Value: %f Expected Unit: %s\n' % (gradient.value, gradient_unit))
 
                     if isinstance(gradient.value, unit.Quantity):
                         gradient_value = gradient.value.to(gradient_unit).magnitude