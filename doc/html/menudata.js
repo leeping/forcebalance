aa9f5a43bb4b38484cfd28587b742ac66b7794fe
Lee-Ping Wang <leeping@ucdavis.edu>
2020-07-09 11:51:31 -0700

    Automatic documentation generation at libra on 07-09-2020 11:51

--- doc/html/menudata.js
+++ doc/html/menudata.js
@@ -1,4 +1,11 @@
+
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
+{text:"Installation",url:"installation.html"},
+{text:"Usage",url:"usage.html"},
+{text:"Tutorial",url:"tutorial.html"},
+{text:"Glossary",url:"glossary.html"},
+{text:"Roadmap",url:"todo.html"},
+{text:"API",url:"api/index.html"},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"}]}]}
