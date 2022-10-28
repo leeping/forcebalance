b65376b129d59cef9bb80de2b22f2a1f340b367d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-28 11:43:29 +0000

    Adding tarfile member sanitization to extractall()

--- src/target.py
+++ src/target.py
@@ -820,7 +820,26 @@ class RemoteTarget(Target):
 
     def get(self,mvals,AGrad=False,AHess=False):
         with tarfile.open("target_result.tar.bz2", "r") as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
         with open('indicate.log', 'r') as f:
             self.remote_indicate = f.read()
         return lp_load('objective.p')
