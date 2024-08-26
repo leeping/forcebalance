a08392c3cf854b5a992c2ca6f884b13223ec0f02
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-08-26 15:33:44 -0500

    Don't build GROMACS, update Python versions

--- src/_version.py
+++ src/_version.py
@@ -51,7 +51,7 @@ def get_config() -> VersioneerConfig:
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
     cfg.versionfile_source = "src/_version.py"
     cfg.verbose = False
